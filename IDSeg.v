814e9c84d3ea04925aac7e4f57fcbe2fdf8dafa7
zLeoAlex <z.alex.leo@outlook.com>
2019-11-19 11:21:49 +0800

    IDSeg Complete

--- IDSeg.v
+++ IDSeg.v
@@ -22,52 +22,46 @@ module IDSeg(
     input clk,
     input rst,
     input [31:0] NPCi,
-    input [31:0] IRi,
+    input [31:0] IR,
 	 input WBFlag,
 	 input [4:0] WBAddr,
 	 input [31:0] WBVal,
-    output reg [31:0] NPCo,
-    output reg [31:0] IRo,
-    output reg [31:0] A,
-    output reg [31:0] B,
-    output reg [31:0] Imm
+    output [31:0] NPCo,
+    output [31:0] IRo,
+    output [31:0] A,
+    output [31:0] B,
+    output [31:0] Imm
     );
-	 
-	 reg [31:0] NPC,IR;
-	 wire [31:0] wA,wB,wImm;
 	 wire imm_s;
+	 reg [31:0] NPC;
 	 
 	 always @ (negedge clk,posedge rst) begin
 		if (rst) begin
 			NPC <= 0;
-			IR <= 0;
 		end else begin
 			NPC <= NPCi;
-			IR <= IRi;
 		end
 	 end
 	 
-	 assign wImm = imm_s ? {IRi[15]?16'hffff:16'h0,IRi[15:0]} : {16'h0,IRi[15:0]};
-	 always @ (posedge clk,posedge rst) begin
-		if (rst) {NPCo,IRo,A,B,Imm} <= 0;
-		else {NPCo,IRo,A,B,Imm} <= {NPCi,IRi,wA,wB,wImm};
-	 end
+	 assign NPCo = NPC;
+	 assign IRo = IR;
+	 assign Imm = {((imm_s && IR[15]) ? 16'hffff:16'h0) , IR[15:0]};
 	 
 	 RegHeap regs(
 		.clk(clk),
 		.rst(rst),
-		.addrA(IRi[25:21]),
-		.addrB(IRi[20:16]),
+		.addrA(IR[25:21]),
+		.addrB(IR[20:16]),
 		.addrW(WBAddr),
 		.dataW(WBVal),
 		.WriteReg(WBFlag),
-		.dataA(wA),
-		.dataB(wB)
+		.dataA(A),
+		.dataB(B)
 	 );
 	 
 	 Controller (
-		.opcode(IRi[31:26]),
-		.funct(IRi[5:0]),
+		.opcode(IR[31:26]),
+		.funct(IR[5:0]),
 		.imm_s(imm_s)
 	 );
 
